181f9e4d8c0fe27f2e334ce57969a7d52f2d6f03
Max Risuhin <risuhin.max@gmail.com>
2019-05-01 21:49:00 +0300

    Ask user to enable Develop mode manually on installation (#47)

--- MsiInstaller/Install-UntrustedAppx.ps1
+++ MsiInstaller/Install-UntrustedAppx.ps1
@@ -4,6 +4,18 @@
     [switch]$EnableSideLoad
 )
 
+# Load assemblies
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+function Is-Developer-Mode-Enabled {
+    $enabled = (test-path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock) -and ((get-item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock).Property.Count -gt 1)
+
+    if($enabled){
+        $enabled = ((get-itemproperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowAllTrustedApps).AllowAllTrustedApps -eq 1) -and ((get-itemproperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense).AllowDevelopmentWithoutDevLicense -eq 1)
+    }
+    return $enabled
+}
+
 if($InstallCert -or $EnableSideLoad){
     if($InstallCert){
         certutil.exe -addstore TrustedPeople "$PSScriptRoot\$packageName.cer"
@@ -11,6 +23,7 @@ if($InstallCert -or $EnableSideLoad){
 
     if($EnableSideLoad){
         set-itemproperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowAllTrustedApps -Value 1 -Verbose
+        set-itemproperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1 -Verbose
     }
     exit 0
 }
@@ -24,11 +37,21 @@ else{
     	exit -1
     }
 
-    $enableSideLoad = (test-path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock) -and ((get-item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock).Property.Count -ne 0)
-
-    if($enableSideLoad){
-        $enableSideLoad = (get-itemproperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowAllTrustedApps).AllowAllTrustedApps -ne 1
+    while (-Not (Is-Developer-Mode-Enabled))
+    {
+       $userChoice = [System.Windows.Forms.MessageBox]::Show("Please enable Developer mode on your device to proceed further. Do you want to update the settings manually?", "Fluent Terminal Installer", [System.Windows.Forms.MessageBoxButtons]::YesNo)
+       if ($userChoice -eq [System.Windows.Forms.DialogResult]::Yes) {
+           [System.Diagnostics.Process]::Start("ms-settings:developers")
+           [System.Windows.Forms.Messagebox]::Show("Please press OK once you enabled Developer mode")
+           continue
+       }
+       if ($userChoice -eq [System.Windows.Forms.DialogResult]::No) {
+           [System.Windows.Forms.Messagebox]::Show("Installer will update HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock automatically to turn on Developer mode")
+           break
+       }
     }
+
+    $enableSideLoad = -Not (Is-Developer-Mode-Enabled)
 
     $trustCert = $PackageSignature.Status -ne "Valid"
 
