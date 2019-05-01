3bda160a18c2882ed4cc996fcf59b25f9bf454ec
Max Risuhin <risuhin.max@gmail.com>
2019-05-02 00:04:22 +0300

    Merge changes from upstream (#48)
    * Move back referencing <None Include="FluentTerminal.App_TemporaryKey.pfx" /> since it's used during AppVeyor GitHub Releases deployment
    
    * Update KeyboardCommandServiceTests.cs (#330)
    
    * Bubble up shell process exit codes (#343)
    
    Make the shell process's exit code available to the main app so that
    actions can be taken depending on whether the shell command was
    successful or not.
    
    Later work will build on this so that failed commands will result in
    tabs being left open when the shell command fails. This will help when
    troubleshooting incorrectly configured profiles, SSH connection issues
    etc.
    
    Exit codes are passed up for both WinPty and ConPty based terminals.

--- FluentTerminal.App.Services/Implementation/TrayProcessCommunicationService.cs
+++ FluentTerminal.App.Services/Implementation/TrayProcessCommunicationService.cs
@@ -16,7 +16,7 @@ namespace FluentTerminal.App.Services.Implementation
         private readonly Dictionary<int, Action<byte[]>> _terminalOutputHandlers;
         private int _nextTerminalId = 0;
 
-        public event EventHandler<int> TerminalExited;
+        public event EventHandler<TerminalExitStatus> TerminalExited;
 
         public TrayProcessCommunicationService(ISettingsService settingsService)
         {
@@ -108,10 +108,9 @@ namespace FluentTerminal.App.Services.Implementation
             else if (messageType == nameof(TerminalExitedRequest))
             {
                 var request = JsonConvert.DeserializeObject<TerminalExitedRequest>(messageContent);
-
                 Logger.Instance.Debug("Received TerminalExitedRequest: {@request}", request);
 
-                TerminalExited?.Invoke(this, request.TerminalId);
+                TerminalExited?.Invoke(this, request.ToStatus());
             }
         }
 
@@ -156,10 +155,7 @@ namespace FluentTerminal.App.Services.Implementation
 
         public Task CloseTerminal(int terminalId)
         {
-            var request = new TerminalExitedRequest
-            {
-                TerminalId = terminalId
-            };
+            var request = new TerminalExitedRequest(terminalId, -1);
 
             Logger.Instance.Debug("Sending TerminalExitedRequest: {@request}", request);
 
