2805070183a27d48305f6959b135d502cc6f5675
Aleksandar Pesic <peske.nis@gmail.com>
2019-05-07 19:48:28 +0200

    Fixing some leftovers after merging.

--- FluentTerminal.App.ViewModels/MainViewModel.cs
+++ FluentTerminal.App.ViewModels/MainViewModel.cs
@@ -85,6 +85,7 @@ namespace FluentTerminal.App.ViewModels
 
             AddLocalShellCommand = new RelayCommand(() => AddTerminal());
             AddRemoteShellCommand = new RelayCommand(() => AddRemoteTerminal());
+            AddTerminalCommand = new RelayCommand(() => AddTerminal());
             ShowAboutCommand = new RelayCommand(ShowAbout);
             ShowSettingsCommand = new RelayCommand(ShowSettings);
 
@@ -132,6 +133,8 @@ namespace FluentTerminal.App.ViewModels
 
         public RelayCommand AddLocalShellCommand { get; }
         public RelayCommand AddRemoteShellCommand { get; }
+
+        public RelayCommand AddTerminalCommand { get; }
 
         public string WindowTitle
         {
