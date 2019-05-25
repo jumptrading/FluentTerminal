d7502da4199137c11f3ca1c5b46eccc87675f334
Max Risuhin <risuhin.max@gmail.com>
2019-05-25 12:24:56 +0300

    Uninstall UWP application on msi uninstaller running (ignore major upgrade mode). (#96)

--- MsiInstaller/Uninstall-UntrustedAppx.ps1
+++ MsiInstaller/Uninstall-UntrustedAppx.ps1
@@ -2,5 +2,10 @@
     [string]$packageName = $null
 )
 
-
-#Remove-AppxPackage $packageName
+$PackageFullName = (Get-AppxPackage $packageName).PackageFullName
+if ($PackageFullName) {
+    Write-Host "Removing package: $packageName"
+    Remove-AppxPackage -package $PackageFullName
+} else {
+    Write-Host "Can't find package: $packageName"
+}
