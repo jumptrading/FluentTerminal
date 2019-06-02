65134dd57eab46fa689191d6ddf3c9575a091ccb
Felix <30559812+felixse@users.noreply.github.com>
2019-06-02 21:44:56 +0200

    Improve IPC (#391)
    * Minimize overhead for buffer IPC
    
    * use byte for terminalId

--- FluentTerminal.App.Services/Terminal.cs
+++ FluentTerminal.App.Services/Terminal.cs
@@ -66,7 +66,7 @@ namespace FluentTerminal.App.Services
 
         public string FallbackTitle { get; private set; }
 
-        public int Id { get; }
+        public byte Id { get; }
 
         /// <summary>
         /// To be called by either view or viewmodel
