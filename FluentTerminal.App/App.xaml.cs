31f62094cc056487b46780b07a16534445ac2601
Max Risuhin <risuhin.max@gmail.com>
2019-07-16 23:17:45 +0300

    Settings option to dump terminal output into file. (#438)
    * Settings option to dump terminal output into file.
    
    * Notify tray process on settings change.

--- FluentTerminal.App/App.xaml.cs
+++ FluentTerminal.App/App.xaml.cs
@@ -519,6 +519,7 @@ namespace FluentTerminal.App
         private void OnApplicationSettingsChanged(object sender, ApplicationSettings e)
         {
             _applicationSettings = e;
+            _trayProcessCommunicationService.UpdateSettings(e);
         }
 
         private void OnMainViewModelClosed(object sender, EventArgs e)
